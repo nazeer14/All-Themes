6fb13396d8a861ece8e6d213624a9bf279d4c1d8
nazeer14 <shaiknazeer141914@gmail.com>
2026-03-10 11:24:51 +0530

    first commit

---
+++ Service-Theme-1/script.js
@@ -0,0 +1,11 @@
+fetch("./footer.html")
+.then(response => response.text())
+.then(data => {
+document.getElementById("footer").innerHTML = data;
+});
+
+fetch("./navbar.html")
+.then(response => response.text())
+.then(data => {
+document.getElementById("navbar").innerHTML = data;
+});
